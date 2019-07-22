a57ab59648f060279ff7c5bf3587f18dfe1d24cf
Ryan Kelly <ryan.kelly.md@gmail.com>
2019-07-21 20:52:47 -0400

    cleanup model building

--- dags/init_airflow.py
+++ dags/init_airflow.py
@@ -13,70 +13,38 @@ args = {
 }
 
 
-def initialize_etl_example():
+def initialize_airflow():
     logging.info('Creating connections, pools, etc.')
 
     session = Session()
 
-    def delete_connection(session, conn_id):
-        """Delete connection by a given conn_id."""
+    # CONNECTIONS
+    new_connection(session,
+        conn_id="my_postgres",
+        conn_type="postgres",
+        host="postgres",
+        port=5432,
+        schema="airflow",
+        login="airflow",
+        password="airflow")
 
-        conn = session.query(models.Connection).filter_by(conn_id=conn_id).first()
-        if conn is None:
-            raise Exception("Connection '%s' doesn't exist" % conn_id)
+    # POOLS
+    new_pool(session, name="mypool", slots=10, description="Allows mox. 10 connections.")
+    # Make sure failure handling works.
+    new_pool(session, name="mypool", slots=10, description="Allows mox. 10 connections.")
 
-        session.delete(conn)
-        session.commit()
-
-        return conn_id
-
-    def create_new_conn(session, attributes):
-
-        new_conn = models.Connection()
-        new_conn.conn_id = attributes.get("conn_id")
-        new_conn.conn_type = attributes.get('conn_type')
-        new_conn.host = attributes.get('host')
-        new_conn.port = attributes.get('port')
-        new_conn.schema = attributes.get('schema')
-        new_conn.login = attributes.get('login')
-        new_conn.set_password(attributes.get('password'))
-
-        session.add(new_conn)
-        session.commit()
-
-    # delete_connection(session, 'postgres_default')
-    create_new_conn(session,
-                    {"conn_id": "my_postgres",
-                     "conn_type": "postgres",
-                     "host": "postgres",
-                     "port": 5432,
-                     "schema": "airflow",
-                     "login": "airflow",
-                     "password": "airflow"})
-
-    new_pool = models.Pool()
-    new_pool.pool = "mypool"
-    new_pool.slots = 10
-    new_pool.description = "Allows max. 10 connections to my_default process."
-
-    session.add(new_pool)
-    session.commit()
-
-    new_chart = models.Chart()
-    new_chart.conn_id = "my_postgres"
-    new_chart.label = "Active Task Instance States"
-    new_chart.sql = """SELECT state, COUNT(*)
+    # CHARTS
+    # FYI ATM graphs in Airflow are broken, use datatables for now.
+    new_chart(session,
+        conn_id='my_postgres',
+        label='Active Task Instances by State',
+        sql="""SELECT state, COUNT(*)
     FROM task_instance
     WHERE state NOT IN ('success', 'failed')
     GROUP BY state
     ORDER BY COUNT(*)
     """
-    new_chart.chart_type = "datatable"
-    new_chart.x_is_date = False
-    new_chart.sql_layout = "columns"
-
-    session.add(new_chart)
-    session.commit()
+    )
 
     session.close()
 
@@ -88,6 +56,74 @@ dag = airflow.DAG(
     max_active_runs=1)
 
 t1 = PythonOperator(task_id='initialize_etl_example',
-                    python_callable=initialize_etl_example,
+                    python_callable=initialize_airflow,
                     provide_context=False,
                     dag=dag)
+
+
+# Helpers
+
+def new_chart(session, conn_id, label, sql, chart_type='datatable', sql_layout="columns", x_is_date=False):
+    logging.info("Creating new chart: {}".format(label))
+    chart = models.Chart()
+
+    chart.conn_id = conn_id
+    chart.label = label
+    chart.sql = sql
+    chart.chart_type = chart_type
+    chart.sql_layout = sql_layout
+    chart.x_is_date = x_is_date
+
+    try:
+        session.add(chart)
+        session.commit()
+    except Exception as err:
+        session.rollback()
+        logging.error(err)
+
+
+def new_connection(session, conn_id, conn_type, host, port, schema, login, password):
+    logging.info("Creating new connection: {}".format(conn_id))
+    connection = models.Connection()
+
+    connection.conn_id = conn_id
+    connection.conn_type = conn_type
+    connection.host = host
+    connection.port = port
+    connection.schema = schema
+    connection.login = login
+    connection.password = password
+
+    try:
+        session.add(connection)
+        session.commit()
+    except Exception as err:
+        session.rollback()
+        logging.error(err)
+
+
+def new_pool(session, name, slots, description):
+    logging.info("Creating new pool: {}".format(name))
+    pool = models.Pool()
+
+    pool.pool = name
+    pool.slots = slots
+    pool.description = description
+
+    try:
+        session.add(pool)
+        session.commit()
+    except Exception as err:
+        session.rollback()
+        logging.error(err)
+
+
+def delete_connection(session, conn_id):
+    """Delete connection by a given conn_id."""
+
+    conn = session.query(models.Connection).filter_by(conn_id=conn_id).first()
+    if conn is None:
+        raise Exception("Connection '%s' doesn't exist" % conn_id)
+
+    session.delete(conn)
+    session.commit()
